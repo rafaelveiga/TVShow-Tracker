a470b6db08d58c2bcbf03b765dfae422c7476a61
rafaelveiga <dsg.rafael@gmail.com>
2016-01-03 02:43:17 -0200

    Adding build tasks into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
+//Production Packages
 var gulp = require('gulp'),
 	connect = require('gulp-connect');
+
+//Build Packages
+var	uglify = require('gulp-uglify'),
+	nano = require('gulp-cssnano'),
+	htmlmin = require('gulp-htmlmin'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant');
 
 	
 // =========================
@@ -21,3 +29,40 @@ gulp.task('watch', function () {
 });
 
 gulp.task('server', ['connect', 'watch']);
+
+// =========================
+// Build
+// =========================
+gulp.task('build-js', function() {
+	gulp.src('./src/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('build-css', function() {
+	gulp.src('./src/css/style.css')
+		.pipe(nano())
+		.pipe(gulp.dest('./build/css/'));
+
+	gulp.src("./src/css/bootstrap-theme.css")
+		.pipe(gulp.dest("./build/css/"));
+});
+
+gulp.task('build-html', function() {
+	gulp.src("./src/*.html")
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest("./build/"));
+});
+
+gulp.task('build-images', function() {
+    gulp.src('src/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('build/img'));
+});
+
+
+gulp.task('build', ['build-js', 'build-css', 'build-html', 'build-images']);
